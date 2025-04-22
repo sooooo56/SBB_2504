a0f1032e498301bb8888c19e47676456ede24dc5
sooooo56 <sooooo4645@gmail.com>
2025-04-22 22:11:40 +0900

    2-4완

---
+++ src/main/java/com/mysite/sbb_2504/Answer.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb_2504;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+}
---
+++ src/main/java/com/mysite/sbb_2504/Sbb2504Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_2504;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb2504Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb2504Application.class, args);
+	}
+
+}
